ec4cb5ff278acdadaa9ba12e2bccd4fa06423bf0
goldenrtiger <xujingyixing@126.com>
2018-02-02 22:42:21 +0800

    fix words limitation bug.

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ app.set('views', __dirname + '/views');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit : "1000kb"}));
+// app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser('secret'));
 app.use(express.static(path.join(__dirname, 'public')));
